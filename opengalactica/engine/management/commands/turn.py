33a75d9cd158d3a25f0179f9923707f5b5f31a9b
HMikiHTH <szubotaj@gmail.com>
2024-08-07 20:28:10 +0100

    battle

--- opengalactica/engine/management/commands/turn.py
+++ opengalactica/engine/management/commands/turn.py
@@ -1,7 +1,21 @@
 from django.core.management.base import BaseCommand, CommandError
 
+from ._battle import Battle
+from engine.models import Fleet
+
 class Command(BaseCommand):
     help = "Execute a turn"
+    
+    def execute_battles(self):
+        self.stdout.write(
+            self.style.SUCCESS('Execute battles...')
+        )
+        under_attack = {*Fleet.objects.filter(distance=0, role="Attackers").values("target")}
+        for planet in under_attack:
+            attackers = Fleet.objects.filter(distance=0, role="Attackers", target=planet)
+            defenders = Fleet.objects.filter(distance=0, role="Defenders", target=planet)
+            battle = Battle(attackers, defenders)
+
 
     def handle(self, *args, **options):
         # Moving new planets
@@ -11,6 +25,7 @@ class Command(BaseCommand):
         # Point and XP recounting
         # Fleet movements
         # Battles
+        self.execute_battles()
         # Increase turn counter
         
         self.stdout.write(
--- opengalactica/engine/models/fleet.py
+++ opengalactica/engine/models/fleet.py
@@ -7,10 +7,12 @@ class Fleet(models.Model):
     name = models.CharField(max_length=128)
     base = models.BooleanField(default=False)
     owner = models.ForeignKey("Planet", on_delete=models.CASCADE, related_name="owner")
-    formation = models.CharField(max_length=8, default="wall")
+    formation = models.CharField(max_length=8, default="wall") # wedge, wall, sphere
     target = models.ForeignKey("Planet", on_delete=models.SET_NULL, related_name="target", null=True)
     distance = models.IntegerField(null=True)
-    task = models.CharField(max_length=8, null=True)
+    turns = models.IntegerField(null=True) # 1 to 3 for attackers, 1 to 6 for defenders
+    task = models.CharField(max_length=8, null=True) # move, return, stand
+    role = models.CharField(max_length=8, default="Defenders") # Defenders, Attackers
 
     @property
     def ships(self):
