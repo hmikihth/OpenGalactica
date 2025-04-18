3cb225d9ee5792f442e7b3a4d7d27003d9ba8955
Miklos <szubotaj@gmail.com>
2025-04-18 03:56:36 +0100

    media directory for file uploads

--- opengalactica/opengalactica/settings.py
+++ opengalactica/opengalactica/settings.py
@@ -132,6 +132,9 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'frontend/react-frontend/build/static'),
 ]
 
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
--- opengalactica/opengalactica/urls.py
+++ opengalactica/opengalactica/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
+from django.conf import settings
+from django.conf.urls.static import static
 
 from game.urls import router
 
@@ -26,3 +28,6 @@ urlpatterns = [
     path('api/v1/', include((router.urls, 'game'), namespace='v1')),
     path('', TemplateView.as_view(template_name='index.html')),
 ]
+
+# Serve media files during development
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
