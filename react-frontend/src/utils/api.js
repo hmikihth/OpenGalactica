e2c961f60a2b387f64c9edbc948f6d9c39f7c6cb
Miklos Horvath <79752640+hmikihth@users.noreply.github.com>
2025-10-20 21:51:11 +0100

    Update API baseURL for production environment

--- react-frontend/src/utils/api.js
+++ react-frontend/src/utils/api.js
@@ -2,7 +2,10 @@
 import axios from 'axios';
 
 const api = axios.create({
+//    Development
 //    baseURL: 'http://localhost:8008/api/v1/',
+
+//    Production
     baseURL: 'http://82.165.219.231:8000/api/v1/',
     withCredentials: true,
 });
